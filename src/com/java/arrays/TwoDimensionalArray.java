6f204e347fade9c496080e81dc1f1cd109bd69cc
rbu9032 <rbu9032@gmail.com>
2025-11-01 22:42:38 +0530

    new learnings for string split & 2D-Array

--- src/com/java/arrays/TwoDimensionalArray.java
+++ src/com/java/arrays/TwoDimensionalArray.java
@@ -20,7 +20,7 @@ public class TwoDimensionalArray {
         //Another approach to create and initialize an 2-d array
         //int arr[][] = {{1,2},{3,4},{5,6}};  /*second approach*/
         //int arr[][] = new int[][]{{1,2},{3,4},{5,6}};  /*third approach*/
-        int arr[][] = new int[3][2];     //fourth approach
+       /* int arr[][] = new int[3][2];     //fourth approach
         arr[0] = new int[]{1, 2};
         arr[1] = new int[]{3, 4};
         arr[2] = new int[]{5, 6};
@@ -47,7 +47,37 @@ public class TwoDimensionalArray {
         for (int i = 0; i < arr.length; i++) {
             size += arr[i].length;
         }
-        System.out.println(size);
+        System.out.println(size);*/
+        System.out.println("Enter row size of Array: ");
+        int x = sc.nextInt();
+        System.out.println("Enter column size of Array: ");
+        int y = sc.nextInt();
+        int[][] arr = new int[x][y];
+        TwoDimensionalArray.print2DArray(arr);
+        System.out.println();
+        System.out.println("Size of the array: " +arr.length);
+    }
+
+    //method to print 2D array
+    static void print2DArray(int[][] arr){
+        Scanner sc1 = new Scanner(System.in);
+        for (int i=0; i< arr.length; i++){
+            for (int j=0; j<arr.length; j++){
+                System.out.println("Enter value at row: "+i+" column: "+j);
+                arr[i][j]=sc1.nextInt();
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.println();
+            for(int j=0; j<arr[i].length;j++){
+                System.out.print(arr[i][j] +", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Printing 2D Array in matrix format: ");
+        for (int[] a: arr){
+            System.out.println(Arrays.toString(a));
+        }
 
     }
 
