8f62ecd01296730ca1dd3fd1f6f772096987bef7
rbu9032 <rbu9032@gmail.com>
2025-12-03 11:34:34 +0530

    first commit

---
+++ src/main/java/orc/Test.java
@@ -0,0 +1,43 @@
+package orc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(Config.class);
+        JdbcTemplate jdbcTemplate = container.getBean("getJdbcTemplate", JdbcTemplate.class);
+       // jdbcTemplate.update("Insert into employee values(?,?,?)",3,"Ravi","ravi@gmail.com");
+        //jdbcTemplate.update("Update employee set empId=? where name=?",2,"Ravi");
+        //jdbcTemplate.update("Delete from employee where empId=2");
+        Employee employee = jdbcTemplate.queryForObject("select * from employee where empId=1", new RowMapper<Employee>() {
+            @Override
+            public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Employee(rs.getInt(1), rs.getString(2), rs.getString(3));
+            }
+        });
+        System.out.println("Use queryForObject to select a single row from the table");
+        System.out.println(employee);
+        List<Employee> employee1 = jdbcTemplate.query("select * from employee", new RowMapper<Employee>() {
+            @Override
+            public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Employee(rs.getInt(1), rs.getString(2), rs.getString(3));
+            }
+        });
+        System.out.println("----------------------------------------------");
+        System.out.println("Use query to select all rows from a table using anonymous inner class");
+        System.out.println(employee1);
+        List<Employee> employee2 = jdbcTemplate.query("select * from employee",(rs, num) -> new Employee(rs.getInt(1 ),rs.getString(2), rs.getString(3)));
+        System.out.println("----------------------------------------------");
+        System.out.println("Use query to select all rows from a table using Lambda");
+        System.out.println(employee2);
+    }
+}
