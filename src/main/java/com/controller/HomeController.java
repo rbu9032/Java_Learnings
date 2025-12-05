5509898a577d8f8920831fcdac809fd8b5b93bbb
rbu9032 <rbu9032@gmail.com>
2025-12-05 22:38:57 +0530

    first commit

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/hi")
+	@ResponseBody
+	public String hi() {
+		//return "Hi";
+	    return "home";
+	}
+	
+	@RequestMapping("/home")
+	public String hi2() {
+		return "home";
+	}
+
+	@RequestMapping("/data")
+	public String home(Model model) {
+		model.addAttribute("name","Raghu");
+		return "home";
+	}
+	@PostMapping("/signup")
+	public String signUp(Model model, HttpServletRequest http) {
+		String email = (String) http.getParameter("email");
+		String password = (String) http.getParameter("password");
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
+		return "data";
+	}
+}
