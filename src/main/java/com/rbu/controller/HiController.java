5b19c9e01d667770489fc959b2db2abee1b8ce4f
rbu9032 <rbu9032@gmail.com>
2025-12-13 23:01:20 +0530

    initial project with spring default and modified user and password in app.prop

---
+++ src/main/java/com/rbu/controller/HiController.java
@@ -0,0 +1,14 @@
+package com.rbu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+	@GetMapping("/hi")
+	public String sayHi() {
+		return "Hi";
+	}
+	
+}
